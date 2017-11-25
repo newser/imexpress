8533de12781a82ae582d82e28c7003b59ce94431
newser <newser@163.com>
2017-11-25 22:03:43 +0800

    dawson function

---
+++ include/special/dawson.h
@@ -0,0 +1,159 @@
+/* Copyright (C) 2017 haniu (niuhao.cn@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+#ifndef __IEXP_DAWSON__
+#define __IEXP_DAWSON__
+
+////////////////////////////////////////////////////////////
+// import header files
+////////////////////////////////////////////////////////////
+
+#include <common/common.h>
+
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_sf_dawson.h>
+
+IEXP_NS_BEGIN
+
+namespace sf {
+
+////////////////////////////////////////////////////////////
+// macro definition
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// type definition
+////////////////////////////////////////////////////////////
+
+template <typename T>
+inline T dawson_impl(const T x)
+{
+    throw std::invalid_argument("todo");
+}
+
+template <>
+inline double dawson_impl(const double x)
+{
+    return gsl_sf_dawson(x);
+}
+
+template <typename T>
+class dawson_functor
+{
+  public:
+    typedef Array<typename T::Scalar,
+                  T::RowsAtCompileTime,
+                  T::ColsAtCompileTime,
+                  T::Flags & RowMajorBit ? RowMajor : ColMajor,
+                  T::MaxRowsAtCompileTime,
+                  T::MaxColsAtCompileTime>
+        ArrayType;
+
+    dawson_functor(const T &x)
+        : m_x(x)
+    {
+    }
+
+    const typename T::Scalar operator()(Index i, Index j) const
+    {
+        return dawson_impl(m_x(i, j));
+    }
+
+  private:
+    const T &m_x;
+};
+
+template <typename T>
+inline CwiseNullaryOp<dawson_functor<T>, typename dawson_functor<T>::ArrayType>
+dawson(const ArrayBase<T> &x)
+{
+    typedef typename dawson_functor<T>::ArrayType ArrayType;
+    return ArrayType::NullaryExpr(x.rows(),
+                                  x.cols(),
+                                  dawson_functor<T>(x.derived()));
+}
+
+template <typename T>
+inline T dawson_e_impl(const T x, T &e)
+{
+    throw std::invalid_argument("todo");
+}
+
+template <>
+inline double dawson_e_impl(const double x, double &e)
+{
+    gsl_sf_result r;
+    if (gsl_sf_dawson_e(x, &r) == GSL_SUCCESS) {
+        e = r.err;
+        return r.val;
+    }
+    THROW_OR_RETURN_NAN(std::runtime_error("dawson"));
+}
+
+template <typename T, typename U>
+class dawson_e_functor
+{
+  public:
+    typedef Array<typename T::Scalar,
+                  T::RowsAtCompileTime,
+                  T::ColsAtCompileTime,
+                  T::Flags & RowMajorBit ? RowMajor : ColMajor,
+                  T::MaxRowsAtCompileTime,
+                  T::MaxColsAtCompileTime>
+        ArrayType;
+
+    dawson_e_functor(const T &x, U &e)
+        : m_x(x)
+        , m_e(e)
+    {
+    }
+
+    const typename T::Scalar operator()(Index i, Index j) const
+    {
+        return dawson_e_impl(m_x(i, j), m_e(i, j));
+    }
+
+  private:
+    const T &m_x;
+    U &m_e;
+};
+
+template <typename T, typename U>
+inline CwiseNullaryOp<dawson_e_functor<T, U>,
+                      typename dawson_e_functor<T, U>::ArrayType>
+dawson(const ArrayBase<T> &x, ArrayBase<U> &e)
+{
+    typedef typename dawson_e_functor<T, U>::ArrayType ArrayType;
+    return ArrayType::NullaryExpr(x.rows(),
+                                  x.cols(),
+                                  dawson_e_functor<T, U>(x.derived(),
+                                                         e.derived()));
+}
+
+////////////////////////////////////////////////////////////
+// global variants
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// interface declaration
+////////////////////////////////////////////////////////////
+}
+
+IEXP_NS_END
+
+#endif /* __IEXP_DAWSON__ */
---
+++ test/case/test_sf_dawson.cpp
@@ -0,0 +1,28 @@
+#include <catch.hpp>
+#include <iostream>
+#include <math/constant.h>
+#include <special/dawson.h>
+#include <test_util.h>
+
+TEST_CASE("test_dawson")
+{
+    iexp::ArrayXd m(4), m2(4), e(4);
+    gsl_sf_result r;
+
+    m << 1.0e-15, 0.5, 2.0, 1000.0;
+    m2 = iexp::sf::dawson(m);
+    REQUIRE(__D_EQ(m2(0, 0), 1.0e-15));
+    REQUIRE(__D_EQ9(m2(1, 0), 0.4244363835020222959));
+    REQUIRE(__D_EQ9(m2(2, 0), 0.30134038892379196603));
+    REQUIRE(__D_EQ9(m2(3, 0), 0.0005000002500003750009));
+
+    m2 = iexp::sf::dawson(m, e);
+    REQUIRE(__D_EQ(m2(0, 0), 1.0e-15));
+    REQUIRE(__D_EQ9(m2(1, 0), 0.4244363835020222959));
+    REQUIRE(__D_EQ9(m2(2, 0), 0.30134038892379196603));
+    REQUIRE(__D_EQ9(m2(3, 0), 0.0005000002500003750009));
+    gsl_sf_dawson_e(m2(0, 0), &r);
+    REQUIRE(__D_EQ9(e(0, 0), r.err));
+    gsl_sf_dawson_e(m2(3, 0), &r);
+    REQUIRE(__D_EQ9(e(3, 0), r.err));
+}
