822e3c73c39216f0547d2ef1483ad1510d6e7077
newser <newser@163.com>
2017-12-19 19:15:53 +0800

    randist: cauchy.h

--- test/case/test_randist.cpp
+++ test/case/test_randist.cpp
@@ -3,6 +3,7 @@
 #include <rand/mul_gauss.h>
 #include <randist/bi_gauss.h>
 #include <randist/bi_gauss.h>
+#include <randist/cauchy.h>
 #include <randist/exp.h>
 #include <randist/expow.h>
 #include <randist/gauss.h>
@@ -231,5 +232,33 @@ TEST_CASE("randist_expow")
     gr.Axis();
     gr.Plot(x, y, "+");
     gr.WriteFrame("expow_pdf.png");
+#endif
+}
+
+TEST_CASE("randist_cauchy")
+{
+    VectorXd v = VectorXd::LinSpaced(10, 0, 3);
+    VectorXd v2 = rdist::cauchy_pdf(v.array(), 2.0);
+
+    Matrix2Xd m = Matrix2Xd::Random(2, 10);
+    Matrix2Xd m2 = rdist::cauchy_pdf(m.array(), 2.0);
+
+    // test compile
+    v2 = rdist::cauchy_pdf(v.array(), 2.0) + rdist::cauchy_pdf(v.array(), 2.0);
+    m2 = rdist::cauchy_pdf(m.array() + m.array(), 2.0);
+
+#if 0 // #ifdef Icauchy_MGL2
+    VectorXd vv = VectorXd::LinSpaced(100, -5, 5);
+    VectorXd vv2 = rdist::cauchy_pdf(vv.array(), 1.0);
+    
+    mglData x(100), y(100);
+    x.Link(vv.data(), vv.size());
+    y.Link(vv2.data(), vv2.size());
+    mglGraph gr;
+    gr.SetOrigin(0, 0);
+    gr.SetRanges(-5, 5, 0, 1);
+    gr.Axis();
+    gr.Plot(x, y, "+");
+    gr.WriteFrame("cauchy_pdf.png");
 #endif
 }
