3eb020aaa0cdfc468c7ce6166e69f0af297f6eb3
newser <newser@163.com>
2017-12-19 17:20:12 +0800

    randist: exp.h

---
+++ include/randist/exp.h
@@ -0,0 +1,132 @@
+/* Copyright (C) 2017 haniu (niuhao.cn@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+#ifndef __IEXP_RANDIST_EXP__
+#define __IEXP_RANDIST_EXP__
+
+////////////////////////////////////////////////////////////
+// import header files
+////////////////////////////////////////////////////////////
+
+#include <common/common.h>
+
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_randist.h>
+
+IEXP_NS_BEGIN
+
+namespace rdist {
+
+////////////////////////////////////////////////////////////
+// macro definition
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// type definition
+////////////////////////////////////////////////////////////
+
+class exp
+{
+  public:
+    exp(const double mu)
+        : m_mu(mu)
+    {
+    }
+
+    double pdf(const double x) const
+    {
+        return gsl_ran_exponential_pdf(x, m_mu);
+    }
+
+    double p(const double x) const
+    {
+        return gsl_cdf_exponential_P(x, m_mu);
+    }
+
+    double invp(const double x) const
+    {
+        return gsl_cdf_exponential_Pinv(x, m_mu);
+    }
+
+    double q(const double x) const
+    {
+        return gsl_cdf_exponential_Q(x, m_mu);
+    }
+
+    double invq(const double x) const
+    {
+        return gsl_cdf_exponential_Qinv(x, m_mu);
+    }
+
+  private:
+    const double m_mu;
+};
+
+template <typename T>
+class exp_pdf_functor
+{
+  public:
+    using ArrayType = Array<typename T::Scalar,
+                            T::RowsAtCompileTime,
+                            T::ColsAtCompileTime,
+                            T::Flags & RowMajorBit ? RowMajor : ColMajor,
+                            T::MaxRowsAtCompileTime,
+                            T::MaxColsAtCompileTime>;
+
+    exp_pdf_functor(const T &x, const typename T::Scalar mu)
+        : m_x(x)
+        , m_exp(mu)
+    {
+    }
+
+    typename T::Scalar operator()(Index i, Index j) const
+    {
+        return m_exp.pdf(m_x(i, j));
+    }
+
+  private:
+    const T &m_x;
+    exp m_exp;
+};
+
+template <typename T>
+inline CwiseNullaryOp<exp_pdf_functor<T>,
+                      typename exp_pdf_functor<T>::ArrayType>
+exp_pdf(const ArrayBase<T> &x, const typename T::Scalar mu)
+{
+    static_assert(TYPE_IS(typename T::Scalar, double),
+                  "scalar can only be double");
+
+    using ArrayType = typename exp_pdf_functor<T>::ArrayType;
+    return ArrayType::NullaryExpr(x.rows(),
+                                  x.cols(),
+                                  exp_pdf_functor<T>(x.derived(), mu));
+}
+
+////////////////////////////////////////////////////////////
+// global variants
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// indexerface declaration
+////////////////////////////////////////////////////////////
+}
+
+IEXP_NS_END
+
+#endif /* __IEXP_RANDIST_EXP__ */
--- test/case/test_randist.cpp
+++ test/case/test_randist.cpp
@@ -3,6 +3,7 @@
 #include <rand/mul_gauss.h>
 #include <randist/bi_gauss.h>
 #include <randist/bi_gauss.h>
+#include <randist/exp.h>
 #include <randist/gauss.h>
 #include <randist/gauss_tail.h>
 #include <randist/mul_gauss.h>
@@ -143,4 +144,32 @@ TEST_CASE("randist_mgauss")
     }
     Matrix2d rcov = rdist::mgauss_cov(rm.array());
     // cout << rcov << endl;
+}
+
+TEST_CASE("randist_exp")
+{
+    VectorXd v = VectorXd::LinSpaced(10, 0, 3);
+    VectorXd v2 = rdist::exp_pdf(v.array(), 2.0);
+
+    Matrix2Xd m = Matrix2Xd::Random(2, 10);
+    Matrix2Xd m2 = rdist::exp_pdf(m.array(), 2.0);
+
+    // test compile
+    v2 = rdist::exp_pdf(v.array(), 2.0) + rdist::exp_pdf(v.array(), 2.0);
+    m2 = rdist::exp_pdf(m.array() + m.array(), 2.0);
+
+#if 0 // #ifdef IEXP_MGL2
+    VectorXd vv = VectorXd::LinSpaced(100, -5, 5);
+    VectorXd vv2 = rdist::exp_pdf(vv.array(), 1.0);
+    
+    mglData x(100), y(100);
+    x.Link(vv.data(), vv.size());
+    y.Link(vv2.data(), vv2.size());
+    mglGraph gr;
+    gr.SetOrigin(0, 0);
+    gr.SetRanges(-5, 5, 0, 1);
+    gr.Axis();
+    gr.Plot(x, y, "+");
+    gr.WriteFrame("exp_pdf.png");
+#endif
 }
