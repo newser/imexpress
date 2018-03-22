2b2c74738b819e042e49e60ccdf70cc00e44c5c9
newser <newser@163.com>
2018-03-22 18:29:42 +0800

    stats: standard deviation

--- test/case/test_stat.cpp
+++ test/case/test_stat.cpp
@@ -2,6 +2,7 @@
 #include <catch.hpp>
 #include <iostream>
 #include <stats/mean.h>
+#include <stats/std.h>
 #include <stats/var.h>
 
 TEST_CASE("stat_mean")
@@ -52,4 +53,35 @@ TEST_CASE("stat_var")
 
     // compile
     v = iexp::stats::var(c + c2.cast<double>());
+    v = iexp::stats::var(c + c2.cast<double>(), 2);
+}
+
+TEST_CASE("stat_std")
+{
+    double v, g;
+
+    iexp::ArrayXd c;
+    c = iexp::ArrayXd::Random(10);
+    v = iexp::stats::std(c);
+    g = gsl_stats_sd(c.data(), 1, c.size());
+    REQUIRE(__D_EQ_IN(v, g, 1e-9));
+
+    c = iexp::ArrayXd::Random(10);
+    v = iexp::stats::std(c, 1);
+    g = gsl_stats_sd_m(c.data(), 1, c.size(), 1);
+    REQUIRE(__D_EQ_IN(v, g, 1e-9));
+
+    iexp::ArrayXi c2;
+    c2 = iexp::ArrayXi::Random(10);
+    v = iexp::stats::std(c2);
+    g = gsl_stats_int_sd(c2.data(), 1, c2.size());
+    REQUIRE(__D_EQ_IN(v, g, 1e-9));
+
+    c2 = iexp::ArrayXi::Random(10);
+    v = iexp::stats::std(c2, 2);
+    g = gsl_stats_int_sd_m(c2.data(), 1, c2.size(), 2);
+    REQUIRE(__D_EQ_IN(v, g, 1e-9));
+
+    // compile
+    v = iexp::stats::std(c + c2.cast<double>());
 }
