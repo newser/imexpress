fe2ce5d3bb15011b649fb0db6c8b6cf1816881ec
newser <newser@163.com>
2018-04-13 13:54:11 +0800

    new multiset

--- include/multiset/multiset.h
+++ include/multiset/multiset.h
@@ -40,8 +40,6 @@ IEXP_NS_BEGIN
 class multiset
 {
   public:
-    using index = size_t;
-
     enum class init
     {
         NONE,
@@ -49,7 +47,7 @@ class multiset
         LAST,
     };
 
-    multiset(const index n, const index k, init i = init::NONE)
+    multiset(size_t n, size_t k, init i = init::NONE)
     {
         m_gm = gsl_multiset_alloc(n, k);
         IEXP_NOT_NULLPTR(m_gm);
@@ -60,11 +58,27 @@ class multiset
         }
     }
 
-    multiset(const multiset &c)
+    template <typename T>
+    multiset(size_t n, const DenseBase<T> &v)
     {
-        m_gm = gsl_multiset_alloc(c.n(), c.k());
+        static_assert(IS_INTEGER(typename T::Scalar), "must be integer type");
+
+        eigen_assert(n >= v.size());
+        m_gm = gsl_multiset_alloc(n, v.size());
         IEXP_NOT_NULLPTR(m_gm);
-        gsl_multiset_memcpy(m_gm, c.m_gm);
+
+        typename type_eval<T>::type e_v(v.eval());
+        copy<size_t, const typename T::Scalar>(gsl_multiset_data(m_gm),
+                                               e_v.data(),
+                                               e_v.size());
+        gsl_multiset_valid(m_gm);
+    }
+
+    multiset(const multiset &rhs)
+    {
+        m_gm = gsl_multiset_alloc(rhs.n(), rhs.k());
+        IEXP_NOT_NULLPTR(m_gm);
+        gsl_multiset_memcpy(m_gm, rhs.m_gm);
     }
 
     multiset(multiset &&c)
@@ -75,30 +89,59 @@ class multiset
 
     ~multiset()
     {
-        gsl_multiset_free(m_gm);
+        if (m_gm != nullptr) {
+            gsl_multiset_free(m_gm);
+        }
     }
 
-    multiset &operator=(const multiset &c)
+    multiset &operator=(const multiset &rhs)
     {
-        eigen_assert((c.n() == n()) && (c.k() == k()));
-        gsl_multiset_memcpy(m_gm, c.m_gm);
+        eigen_assert((rhs.n() == n()) && (rhs.k() == k()));
+        gsl_multiset_memcpy(m_gm, rhs.m_gm);
         return *this;
     }
 
-    multiset &operator=(const multiset &&c)
+    multiset &operator=(multiset &&rhs)
     {
-        memcpy(m_gm, c.m_gm, sizeof(gsl_multiset));
-        memset(c.m_gm, 0, sizeof(gsl_multiset));
+        if (m_gm != nullptr) {
+            gsl_multiset_free(m_gm);
+        }
+        m_gm = rhs.m_gm;
+        rhs.m_gm = nullptr;
         return *this;
     }
 
-    index &operator[](const index i)
+    template <typename T>
+    multiset &operator=(const DenseBase<T> &v)
+    {
+        static_assert(IS_INTEGER(typename T::Scalar), "must be integer type");
+
+        size_t n = gsl_multiset_n(m_gm);
+        if (n < v.size()) {
+            throw std::invalid_argument("invalid input size");
+        }
+
+        size_t k = gsl_multiset_k(m_gm);
+        if (k == v.size()) {
+            typename type_eval<T>::type e_v(v.eval());
+            copy<size_t, const typename T::Scalar>(gsl_multiset_data(m_gm),
+                                                   e_v.data(),
+                                                   k);
+            gsl_multiset_valid(m_gm);
+        } else {
+            // rvalue assignment
+            *this = multiset(n, v);
+        }
+        return *this;
+    }
+
+    size_t &operator[](size_t i) const
     {
         eigen_assert(i < k());
         return *(&gsl_multiset_data(m_gm)[i]);
     }
 
-    index &operator()(const index i)
+    size_t &operator()(size_t i) const
     {
         eigen_assert(i < k());
         return *(&gsl_multiset_data(m_gm)[i]);
@@ -114,12 +157,12 @@ class multiset
         gsl_multiset_init_last(m_gm);
     }
 
-    index n() const
+    size_t n() const
     {
         return gsl_multiset_n(m_gm);
     }
 
-    index k() const
+    size_t k() const
     {
         return gsl_multiset_k(m_gm);
     }
@@ -129,30 +172,106 @@ class multiset
         return bool(gsl_multiset_next(m_gm) == GSL_SUCCESS);
     }
 
-    bool next(multiset &c) const
-    {
-        c = *this;
-        return c.next();
-    }
-
     bool prev()
     {
         return bool(gsl_multiset_prev(m_gm) == GSL_SUCCESS);
     }
 
-    bool prev(multiset &c) const
-    {
-        c = *this;
-        return c.prev();
-    }
-
     bool valid() const
     {
-        return bool(gsl_multiset_valid(m_gm) == GSL_SUCCESS);
+        try {
+            gsl_multiset_valid(m_gm);
+            return true;
+        } catch (...) {
+            return false;
+        }
     }
 
   private:
     gsl_multiset *m_gm;
+
+    // to matrix
+    template <typename _Scalar, int _Rows, int _Cols>
+    class matrix_functor
+    {
+      public:
+        matrix_functor(const multiset &ms)
+            : m_ms(ms)
+        {
+        }
+
+        _Scalar operator()(Index i, Index j) const
+        {
+            // always colume major
+            return static_cast<_Scalar>(m_ms(i + j * _Rows));
+        }
+
+      private:
+        const multiset &m_ms;
+    };
+
+    // to array
+    template <typename _Scalar, int _Rows, int _Cols>
+    class array_functor
+    {
+      public:
+        array_functor(const multiset &ms)
+            : m_ms(ms)
+        {
+        }
+
+        _Scalar operator()(Index i, Index j) const
+        {
+            // always colume major
+            return static_cast<_Scalar>(m_ms(i + j * _Rows));
+        }
+
+      private:
+        const multiset &m_ms;
+    };
+
+  public:
+    template <typename _Scalar, int _Rows, int _Cols>
+    inline CwiseNullaryOp<matrix_functor<_Scalar, _Rows, _Cols>,
+                          Matrix<_Scalar, _Rows, _Cols>>
+    matrix()
+    {
+        static_assert(IS_INTEGER(_Scalar), "must be integer type");
+        static_assert((_Rows != Dynamic) && (_Cols != Dynamic),
+                      "can not be dynamic size");
+
+        size_t k = gsl_multiset_k(m_gm);
+        if (k != _Rows * _Cols) {
+            throw std::invalid_argument("invalid matrix size");
+        }
+
+        using MatrixType = Matrix<_Scalar, _Rows, _Cols>;
+        return MatrixType::NullaryExpr(_Rows,
+                                       _Cols,
+                                       matrix_functor<_Scalar, _Rows, _Cols>(
+                                           *this));
+    }
+
+    template <typename _Scalar, int _Rows, int _Cols>
+    inline CwiseNullaryOp<array_functor<_Scalar, _Rows, _Cols>,
+                          Array<_Scalar, _Rows, _Cols>>
+    array()
+    {
+        static_assert(IS_INTEGER(_Scalar), "must be integer type");
+        static_assert((_Rows != Dynamic) && (_Cols != Dynamic),
+                      "can not be dynamic size");
+
+        size_t k = gsl_multiset_k(m_gm);
+        if (k != _Rows * _Cols) {
+            throw std::invalid_argument("invalid array size");
+        }
+
+        using ArrayType = Array<_Scalar, _Rows, _Cols>;
+        return ArrayType::NullaryExpr(_Rows,
+                                      _Cols,
+                                      array_functor<_Scalar, _Rows, _Cols>(
+                                          *this));
+    }
 };
 
 ////////////////////////////////////////////////////////////
@@ -160,7 +279,7 @@ class multiset
 ////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////
-// indexerface declaration
+// size_terface declaration
 ////////////////////////////////////////////////////////////
 
 IEXP_NS_END
--- test/case/test_multiset.cpp
+++ test/case/test_multiset.cpp
@@ -57,3 +57,83 @@ TEST_CASE("test_multiset")
     REQUIRE(c4(0) == 0);
     REQUIRE(c4(1) == 1);
 }
+
+TEST_CASE("test_multiset2")
+{
+    Array<int, Dynamic, Dynamic, RowMajor> a(3, 2), a0(3, 2);
+    a << 0, 1, 2, 3, 4, 5;
+    multiset c(7, a);
+    REQUIRE(c[0] == 0);
+    REQUIRE(c[5] == 5);
+
+    a0.setZero();
+    multiset c0(7, a + a0);
+    REQUIRE(c0[0] == 0);
+    REQUIRE(c0[5] == 5);
+
+    Matrix<int, 2, 3> m = c.matrix<int, 2, 3>();
+    REQUIRE(m(0, 0) == 0);
+    REQUIRE(m(1, 2) == 5);
+
+    a.resize(2, 1);
+    a(0, 0) = 1;
+    a(1, 0) = 2;
+    c = a;
+    REQUIRE(c[0] == 1);
+    REQUIRE(c[1] == 2);
+
+    // same size
+    a(1, 0) = 3;
+    c = a;
+    REQUIRE(c[1] == 3);
+
+    // too large
+    except_begin()
+    {
+        a.resize(9, 9);
+        c = a;
+    }
+    except_str("invalid input size");
+
+    // matrix
+
+    Matrix<int, Dynamic, Dynamic> b(3, 2), b0(3, 2);
+    b << 0, 3, 1, 4, 2, 5;
+    multiset c2(7, b);
+    REQUIRE(c2[0] == 0);
+    REQUIRE(c2[5] == 5);
+
+    b0.setZero();
+    multiset c20(7, b + b0);
+    REQUIRE(c20[0] == 0);
+    REQUIRE(c20[5] == 5);
+
+    Array<unsigned char, 2, 3> ar = c2.matrix<unsigned char, 2, 3>();
+    REQUIRE(ar(0, 0) == 0);
+    REQUIRE(ar(1, 2) == 5);
+
+    b.resize(2, 1);
+    b(0, 0) = 1;
+    b(1, 0) = 2;
+    c2 = b;
+    REQUIRE(c2[0] == 1);
+    REQUIRE(c2[1] == 2);
+
+    b0.resize(2, 1);
+    c20 = b + b0;
+    REQUIRE(c20[0] == 1);
+    REQUIRE(c20[1] == 2);
+
+    // same size
+    b(1, 0) = 3;
+    c2 = b;
+    REQUIRE(c2[1] == 3);
+
+    // too large
+    except_begin()
+    {
+        b.resize(9, 9);
+        c2 = b;
+    }
+    except_str("invalid input size");
+}
